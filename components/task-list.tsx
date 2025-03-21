d0792f9d0153d7fd4ed083c0098eb7c99264c7f5
Paul Ernest Kachule <58757588+Admin368@users.noreply.github.com>
2025-03-21 23:08:54 +0800

    Add task updates

--- components/task-list.tsx
+++ components/task-list.tsx
@@ -405,6 +405,19 @@ export function TaskList({
                 <ArrowUpDown className="h-3 w-3 md:h-4 md:w-4 mr-1 md:mr-2" />
                 {showReorderButtons ? "Hide Reorder" : "Reorder"}
               </Button>
+              {isAdmin && (
+                <Button
+                  variant="outline"
+                  size="sm"
+                  onClick={() => {
+                    setEditingTask(null);
+                    setShowTaskDialog(true);
+                  }}
+                >
+                  <Plus className="h-4 w-4 mr-2" />
+                  Add Task
+                </Button>
+              )}
             </div>
           </div>
           {!checkInStatus?.checkedIn && (
